af7bdb316109c6b040c42309806ab583b0050ea5
xmc811 <mingchux@bcm.edu>
2014-09-22 22:02:01 -0500

    kobe simulation data

--- probability/basic_simulation.R
+++ probability/basic_simulation.R
@@ -1,5 +1,21 @@
+
+
+## Use simulation to analyze the independence of a random variable
+## Kobe's shot as an example
+
 load(url("http://www.openintro.org/stat/data/kobe.RData"))
 # load database instead of loading r file
+
+head(kobe)
+
+kobe_streak <- calc_streak(kobe$basket)
+barplot(table(kobe_streak))
+
+#calc_streak is customized function to count the streak of one outcome in one random variable
+
+
+
+## basic simulation in R
 
 outcomes <- c("heads", "tails")
 sample(outcomes, size = 10, replace = TRUE)
@@ -8,4 +24,9 @@ sample(outcomes, size = 10, replace = TRUE)
 outcomes <- c("H", "M")
 sim_basket <- sample(outcomes, size = 100, replace = TRUE, prob=c(0.45,0.55))
 barplot(table(calc_streak(sim_basket)))
-# another simulation with plot
+# another simulation with unequal probability and plot the result
+# simulate Kobe's shot in R
+
+
+
+
