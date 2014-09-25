1990d18633f6a8e0fde0df87592065ac90b65132
Mingchu Xu <mingchux@bcm.edu>
2014-09-25 01:53:18 -0500

    some revisions

--- bioinformatics/unique_count.R
+++ bioinformatics/unique_count.R
@@ -4,24 +4,29 @@ id_mt = read.csv("C:/Users/HGSC/Desktop/Shell script/id_mt.csv",header = FALSE)
 id_total = read.csv("C:/Users/HGSC/Desktop/Shell script/id_total.csv",header = FALSE)
 dim(id_total)
 
-
+## Background
+## The csv files contain NGS reads mapped to whole genome and mitochondria
+## We want to know if the mitochondria reads also mapped to the whole genome, if so, how many?
 
 ## This block returns the true result
-## check if elements in list A is also in List B
+## The basic algorithmic question is check if elements in list A is also in List B and count how many
+
 sum=0
-y=id_mt[1:108,1]
+y=id_mt[1:108,1]    # generate a list A
 for (i in y){
-       count = sum(id_total[1]==i)
-       if (count>0){
-              sum=sum+1
+       count = sum(id_total[1]==i)           # count times that reads appear
+       if (count>1){                         # 1 is the threshold because list A is a subset of list B
+              sum=sum+1                      # counter for final calculation
        }
 }
 
-sum
-# get the totol "positive" results
+sum                   
 
 
-## This block returns a test result
+# get the total "positive" hits
+
+
+## This block only returns a test result
 sum=0
 x=id_mt[,1]
 for (i in x){
