0a32059388081413bad1ee1ed4cbdda25bb3a86e
Mingchu Xu <mingchux@bcm.edu>
2014-09-29 02:12:29 -0500

    add invisible function

--- debug/basic_info_functions.R
+++ debug/basic_info_functions.R
@@ -9,12 +9,23 @@ divider <- function(a,b) {                       # declare two parameters a and 
        if (is.numeric(a) & is.numeric(b)) {      # check if a and b are numbers
               message("read a and b")            # if they are, make a message
               if (b==0) {                        # if b is 0, calculate anyway
-                     c<-a/b                      # but make a warning
-                     print(c)
+                     d<<-a/b                     # but make a warning
+                     
+                     ## Notice: here we use "<<" rather than ""< for assignment
+                     ## since we want d become a global variable instead of
+                     ## a local variable which only appears in the function-calling env
+                     
+                     invisible(d)
+                     ## the function returns the d value but not print it
+                     ## if d is put after that
+                     ## d is still printed
+                     
                      warning("b is 0")
               } else {                           # if b is not 0
-                     c<-a/b                      # do the calculation as usual
-                     print(c)
+                     d<<-a/b                      # do the calculation as usual
+                     invisible(d)
+                     ## the same as above
+                     
                      message("result calculated")# return a normal message
               }
        } else {                                  # if both are not numbers
