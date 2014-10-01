50ca139245512b638c352254ec656c85a2708ec1
Mingchu Xu <mingchux@bcm.edu>
2014-10-01 13:38:29 -0500

    integrate simulation

--- probability/basic_simulation.R
+++ probability/basic_simulation.R
@@ -30,3 +30,65 @@ barplot(table(calc_streak(sim_basket)))
 
 
 
+
+
+## simulation with distributions
+
+x <- rnorm(20,mean=0,sd=2)
+# a very basic simulation of normal distributed population
+
+## dnorm, pnorm functions etc. can be found in z-statistics.R
+
+y <- rpois(100,lambda=2)
+## simulation of Poisson distribution, where lambda is the mean value
+
+set.seed(7)
+x <- rpois(10,2)
+## output [1] 6 1 0 0 1 3 1 5 1 2
+## set.seed functio fixes the seed of random variable generation process
+## thus allowing people to generate exactly the same outcomes
+
+x <- rpois(10,2)
+## output [1] 1 1 3 0 2 0 2 0 6 1
+## seed is changed after last random sampling
+
+set.seed(7)
+x <- rpois(10,2)
+## output [1] 6 1 0 0 1 3 1 5 1 2
+## the same dataset generated
+
+
+
+
+## simulation of a linear model
+
+## y=ax+b+e
+
+x <- rnorm(10,500,200)
+e <- rnorm(10,0,1)
+
+y <- 2*x+10+e
+
+plot(x,y)
+cor(x,y)
+
+## this linear model can be played around for understanding the x,y correlation
+
+## the smaller e variation, the larger correlation, given others unchanged
+## the larger the y value (mean of x or coefficient of y), the larger correlation
+## the larger x variation, the larger correlation
+
+
+## more real random variables
+## error distribution is Poisson instead of normal
+
+x <- rnorm (100,0,1)
+log.mu <- 0.5+x
+y <- rpois(100,exp(log.mu))
+
+plot(x,y)
+
+
+
+
+
