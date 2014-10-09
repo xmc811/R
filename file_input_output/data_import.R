d73c3fde3bd09a1ac232ed01bbe75dc9937d84d0
xmc811 <mingchux@bcm.edu>
2014-10-09 00:19:28 -0500

    update data importing

--- file_input_output/data_import.R
+++ file_input_output/data_import.R
@@ -30,3 +30,37 @@ list.files("./GetData/")
 dateDownloaded<-date()
 
 # this is used to record the download time
+
+
+camaraData<- read.table("./GetData/camara.csv",sep=",",header=TRUE)
+# this is the most robust and flexible function to be used for data importing
+# rather than read.csv
+# however in csv files, it is delimited by comma instead of tab
+# if we don't specify the separation, error will occur
+# here we use comma
+
+# if using read.csv, it automatically sets seq to be comma and header to be TRUE
+
+# there are also some important parameters to choose
+
+# nrows and skip: choose or skip first n rows
+# na.strings: set a series of string to be NA, including black ("")
+# quote is used to eliminate quoting problems (some strange behaviours in R)
+
+xlsxUrl<-c("https://data.baltimorecity.gov/api/views/dz54-2aru/rows.xlsx?accessType=DOWNLOAD")
+
+download.file(xlsxUrl,destfile="./GetData/camara.xlsx")
+
+datexlsxDownloaded<-date()
+
+
+
+libary(xlsx)
+
+read.xlsx("./GetData/camara.csv",sheetIndex=1,header=TRUE)
+# this function is used to read data from xlsx, contains some useful parameters
+
+
+
+
+
